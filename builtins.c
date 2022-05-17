4d1971a5b03f2e8bc27f821dcf165c2b4043f28e
bismarkb609 <bismarkb609@gmail.com>
2022-05-17 13:06:52 -0700

    builtins fil

---
+++ builtins.c
@@ -0,0 +1,9 @@
+#include "rktsh.h"
+/**
+ * _num_builtins - returns number of builtins
+ * Return: number of builtins.
+ */
+int _num_builtins(void)
+{
+return (sizeof(builtin_str) / sizeof(char *));
+}
